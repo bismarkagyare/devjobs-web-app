cd03cea90e95bee0c856af991b5d500e947c9255
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-07 12:47:28 +0000

    Import setupSearch function from filterJobs file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles/style.scss';
 import { fetchAndRenderJobListings } from './modules/jobListings';
+import { setupSearch } from './modules/filterJobs';
 
-window.addEventListener('DOMContentLoaded', fetchAndRenderJobListings);
+window.addEventListener('DOMContentLoaded', () => {
+  fetchAndRenderJobListings();
+  setupSearch();
+});
