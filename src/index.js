fc9c03fa6ebc176ad7ae415a20e517ebea2dfdb8
Bismark Agyare <bismarkagyare54@gmail.com>
2023-07-08 14:02:23 +0000

    Import displayJobDetails function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/style.scss';
 import { fetchAndRenderJobListings } from './modules/jobListings';
 import { setupSearch } from './modules/filterJobs';
+import displayJobDetails from './modules/jobDetails';
 
 window.addEventListener('DOMContentLoaded', () => {
   fetchAndRenderJobListings();
   setupSearch();
+  displayJobDetails();
 });
